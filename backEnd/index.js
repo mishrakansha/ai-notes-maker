d38c6d989aa197fb3d4b985d17cf669293c78696
akansha mishra <104217598+mishrakansha@users.noreply.github.com>
2026-04-21 15:25:40 +0530

    5th commit

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from "./routes/authRoutes.js";
+import studyRoutes from "./routes/studyRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 app.use("/api/auth", authRoutes);
+app.use("/api/study", studyRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
