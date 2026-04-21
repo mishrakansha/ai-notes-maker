de200cdd9cde1e2daac2a5ad95d77067da41f5a0
akansha mishra <104217598+mishrakansha@users.noreply.github.com>
2026-04-21 13:49:50 +0530

    3rd commit

--- backEnd/controllers/authController.js
+++ backEnd/controllers/authController.js
@@ -1,4 +1,4 @@
-import { registerUser } from "../services/authService.js";
+import { registerUser, loginUser} from "../services/authService.js";
 
 export const register = async (req, res) => {
   try {
@@ -9,3 +9,15 @@ export const register = async (req, res) => {
     res.status(400).json({ error: error.message || "Registration Failed" });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const data = await loginUser(req.body);
+    res.json(data);
+  }
+  catch (error) {
+    res.status(400).json({
+      error: error.message || "Login failed"
+    });
+  }
+}
